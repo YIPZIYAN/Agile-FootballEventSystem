f41cdecfb0905b6c2ad8812fb49d4e8e59cca9e3
YIPZIYAN <103913961+YIPZIYAN@users.noreply.github.com>
2024-04-06 00:57:18 +0800

    done create merchandise form UI design

--- resources/views/merchandise/create.blade.php
+++ resources/views/merchandise/create.blade.php
@@ -41,33 +41,33 @@
                                 <x-input-error class="mt-2" :messages="$errors->get('price')" />
                             </div>
 
-
-
+                            <div>
+                                <x-input-label for="image" :value="__('Upload Image')" />
+                                <input
+                                    class="mt-1 block w-full text-sm text-gray-900 border border-gray-300 rounded-lg cursor-pointer bg-gray-50 dark:text-gray-400 focus:outline-none dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400"
+                                    aria-describedby="file_input_help" id="image" name="image"
+                                    :value="old('image')" type="file" required>
+                                <p class="mt-1 text-sm text-gray-500 dark:text-gray-300" id="file_input_help">PNG, JPG
+                                    or JPEG.</p>
+                            </div>
 
                             <div>
                                 <x-input-label for="stock_quantity" :value="__('Available Stock Quantity')" />
-                                <x-text-input id="stock_quantity" name="stock_quantity" type="number"
+                                <x-text-input id="stock_quantity" name="stock_quantity" type="number" min="0"
                                     class="mt-1 block w-full" :value="old('stock_quantity')" required autofocus />
                                 <x-input-error class="mt-2" :messages="$errors->get('stock_quantity')" />
                             </div>
 
-                            <div class="flex items-center justify-center w-full">
-                                <label for="image"
-                                    class="flex flex-col items-center justify-center w-full h-64 border-2 border-gray-300 border-dashed rounded-lg cursor-pointer bg-gray-50 dark:hover:bg-bray-800 dark:bg-gray-700 hover:bg-gray-100 dark:border-gray-600 dark:hover:border-gray-500 dark:hover:bg-gray-600">
-                                    <div class="flex flex-col items-center justify-center pt-5 pb-6">
-                                        <svg class="w-8 h-8 mb-4 text-gray-500 dark:text-gray-400" aria-hidden="true"
-                                            xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 16">
-                                            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"
-                                                stroke-width="2"
-                                                d="M13 13h3a3 3 0 0 0 0-6h-.025A5.56 5.56 0 0 0 16 6.5 5.5 5.5 0 0 0 5.207 5.021C5.137 5.017 5.071 5 5 5a4 4 0 0 0 0 8h2.167M10 15V6m0 0L8 8m2-2 2 2" />
-                                        </svg>
-                                        <p class="mb-2 text-sm text-gray-500 dark:text-gray-400"><span
-                                                class="font-semibold">Click to upload</span> or drag and drop</p>
-                                        <p class="text-xs text-gray-500 dark:text-gray-400">SVG, PNG, JPG or GIF (MAX.
-                                            800x400px)</p>
-                                    </div>
-                                    <input id="image" name="image" type="file" class="hidden" />
-                                </label>
+                            <div>
+                                <x-input-label for="category" :value="__('Category')" />
+                                <select id="category"
+                                    class="mt-1 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+                                    <option selected>Choose a cateogry</option>
+                                    <option value="cap">Cap</option>
+                                    <option value="bag">Bag</option>
+                                    <option value="poster">Poster</option>
+                                    <option value="other">Other</option>
+                                </select>
                             </div>
 
                             <div class="flex items-center gap-4">
