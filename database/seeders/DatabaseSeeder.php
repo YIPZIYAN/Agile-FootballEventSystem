b13c82ec94a435f591090c14864f84c006800d59
limshilei <118789655+limshilei@users.noreply.github.com>
2024-04-06 18:17:45 +0800

    Update DatabaseSeeder.php

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 
 use App\Models\Role;
+use App\Models\User;
 use Illuminate\Database\Seeder;
 use Spatie\Permission\Models\Role as ModelsRole;
 
@@ -16,9 +17,15 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         // \App\Models\User::factory(10)->create();
-        $user = \App\Models\User::factory()->create([
+        $user = User::factory()->create([
             'name' => 'admin',
             'email' => 'admin@gmail.com',
+            'password' => 'password',
+        ]);
+
+        User::create([
+            'name' => 'Jason',
+            'email' => 'jason@gmail.com',
             'password' => 'password',
         ]);
         ModelsRole::create(['name' => 'admin']);
